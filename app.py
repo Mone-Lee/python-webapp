00e8bc8c2645fa5eec5bbd59c97aea768efdd337
limengyi <1051933474@qq.com>
2021-04-08 16:24:09 +0800

    chore: 简化webapp骨架代码

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio
+from datetime import datetime
 
 from aiohttp import web
 
 def index(request):
-	return web.Response(body=b'<h1>python webapp</h1>', content_type='text/html')
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
 
-async def init(loop):
-	app = web.Application(loop=loop)
-	app.router.add_route('GET', '/', index)
-	server = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-	logging.info('server started at http://127.0.0.1:9000')
-	return server
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+app = web.Application()
+app.router.add_route('GET', '/', index)
+logging.info('Server is running at http://0.0.0.0:8080/ ...')
 
+web.run_app(app)
