c57b64e47450b49b2877f20bf83d0f50e21130d0
limengyi <1051933474@qq.com>
2021-05-24 16:32:06 +0800

    feat: 添加web框架文件coroweb.py, 处理url请求

---
+++ www/coroweb.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import functools, inspect, logging, os, asyncio
+
+from aiohttp import web
+
+from urllib import parse
+
+from apis import APIError
+
+def get(path):
+    '''
+    Define decorator @get('/path')
+    例： 
+    @get('/blog/{id}')
+    def get_blog(id):
+        pass
+
+    在把装饰器放在get_blog定义处，等同于执行了语句： get_blog = get('/blog/{id}')(get_blog)
+    如果是不带参数的情况，则装饰器不需要def decorator这一层, 调用时则等同于： get_blog = get(get_blog)
+    '''
+    def decorator(func):
+        @functools.wraps(func)      # @functools.wraps能使装饰器处理后的函数拥有原函数相同的名称__name__和函数属性, 如果没有，则__name__就变成wrapper
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+def post(path):
+    '''
+    Define decorator post('/path')
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+def get_required_kw_args(fn):
+    args = []
+    # inspect.signature函数可以获得函数fn的所有参数
+    # 例：
+    # def foo(a, *, b:int, **kwargs):
+    #     pass
+    # sig = inspect.signature(foo)
+    # str(sig)                          # (a, *, b:int, **kwargs)
+    # str(sig.parameters['b'])          # 'b:int'
+    # sig.parameters['b'].annotation    # <class: 'int'>
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        # args只存储kw类型且没有设置默认值的参数
+        # 例：
+        # def foo(a, b, *, c, d=10):   则最后args只保留c， 因为a,b都不是kw类型的，而d有默认值
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+def has_named_kw_args(fn):
+    params = inspect.signature(fn).parameters
+    for _, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for _, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:     # **kwargs参数
+            return True
+
+def has_request_arg(fn):
+    params = inspect.signature(fn).parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
+
+# RequestHandler: URL处理函数
+# 虽然是一个类，由于定义了__call__方法，可以将它的实例视为函数
+# RequestHandler从URL函数中分析其需要接收的参数
+# 从request中获取必要的参数，调用URL函数，然后把结果转换为web.response对象，以符合aiohttp框架的要求
+class RequestHandler(object):
+
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_names_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
+    
+    async def __call__(self, request):
+        kw = None
+        if self._has_var_kw_arg or self._has_names_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-Type')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                # remove all unamed kw:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            # check named arg:
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        # check required kw:
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = await self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
+
+# add_route函数，用来注册一些url处理函数
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method, path, RequestHandler(app, fn))
+
+# 把很多次add_route()注册的调用变成自动扫描
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
+
+        
+
+    
