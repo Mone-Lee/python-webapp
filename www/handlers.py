6258910e0dc4412215462fe2b5b2b53bddfa20b4
limengyi <1051933474@qq.com>
2021-05-26 11:36:27 +0800

    feat: 编写MVC (handlers.py)

--- www/handlers.py
+++ www/handlers.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-' url handlers'
+from coroweb import get, post
+
+from models import User
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    print('index === ')
+    print(users)
+    return {
+        '__template__': 'index.html',
+        'users': users
+    }
